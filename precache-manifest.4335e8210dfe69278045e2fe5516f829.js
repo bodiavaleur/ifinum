2c780fd391f6d50823ee6675712a69ed15168a7a
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2019-02-14 16:41:31 +0200

    Updates

---
+++ precache-manifest.4335e8210dfe69278045e2fe5516f829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ccec526c99485fecb2e",
+    "url": "/ifinum/static/js/runtime~main.6ccec526.js"
+  },
+  {
+    "revision": "51b757ec6fb3efbd2e76",
+    "url": "/ifinum/static/js/main.51b757ec.chunk.js"
+  },
+  {
+    "revision": "4487e7c3bd485c660470",
+    "url": "/ifinum/static/js/1.4487e7c3.chunk.js"
+  },
+  {
+    "revision": "51b757ec6fb3efbd2e76",
+    "url": "/ifinum/static/css/main.d7b8b3f9.chunk.css"
+  },
+  {
+    "revision": "dff29e11cb8c70575e9610a1dcd58c91",
+    "url": "/ifinum/index.html"
+  }
+];
